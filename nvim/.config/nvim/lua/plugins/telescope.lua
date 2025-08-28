8a182b3a7211e0f02ec191692acc66b3d838142b
moeezali2375 <moeezali2375@gmail.com>
2025-08-28 11:54:57 +0500

    add keymaps for yanking, selecting whole buffer

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -102,6 +102,12 @@ return {
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = '[ ] Find existing [B]uffers' })
+    vim.keymap.set('n', '<leader>ya', function()
+      vim.cmd '%y+'
+    end, { desc = 'Yank whole buffer' })
+
+    vim.keymap.set('n', '<leader>va', '0ggVG$', { desc = 'Select whole buffer' })
+
     local diagnostics_enabled = true
 
     vim.keymap.set('n', '<leader>vd', function()
