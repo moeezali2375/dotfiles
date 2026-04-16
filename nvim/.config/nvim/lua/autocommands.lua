8eedfb2de67fe835f0c712bd8f2e337444d4a9f7
moeezali2375 <moeezali2375@gmail.com>
2026-04-16 12:31:24 +0500

    csv support

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -6,4 +6,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Create a group so the autocmd doesn't duplicate on reload
+local csv_group = vim.api.nvim_create_augroup('csv_auto_view', { clear = true })
 
+vim.api.nvim_create_autocmd('FileType', {
+  group = csv_group,
+  pattern = { 'csv', 'tsv' },
+  callback = function()
+    -- We use pcall (protected call) to prevent errors if the plugin isn't installed
+    pcall(vim.cmd, 'CsvViewEnable')
+  end,
+  desc = 'Automatically enable csvview for CSV and TSV files',
+})
